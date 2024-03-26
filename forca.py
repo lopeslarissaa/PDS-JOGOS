9a0d5947d029e3b9cf3dc1e27fa75f91b1870fa9
Larissa <130561679+lopeslarissaa@users.noreply.github.com>
2024-03-26 09:25:10 -0300

    forca aula 1

---
+++ forca.py
@@ -0,0 +1,14 @@
+def jogar():
+ print("****************")
+ print("***Larissa Lopes de Carvalho***")
+ print("****************")
+
+ palavra_secreta = "maça".upper()
+ letra_secreta = ["_"  for letra in palavra_secreta]
+ letras_acertadas2 = []
+ 
+ for letra in (palavra_secreta)
+ 
+
+
+
